bda24a5ea71d05280ad3717010b6b0e76776a259
jyothishnarayan <jyothishn@gmail.com>
2024-08-13 07:01:23 +0530

    DataBinding replaced findViewById

--- app/src/main/java/com/anushka/bindingdemo1/MainActivity.kt
+++ app/src/main/java/com/anushka/bindingdemo1/MainActivity.kt
@@ -8,26 +8,36 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.anushka.bindingdemo1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val button = findViewById<Button>(R.id.submit_button)
+        //val button = findViewById<Button>(R.id.submit_button)
+        val button = binding.submitButton
         button.setOnClickListener {
             displayGreeting()
         }
     }
     private fun displayGreeting() {
-        val messageView = findViewById<TextView>(R.id.greeting_text_view)
+        binding.apply {
+            val messageView = greetingTextView
+            val nameText = nameEditText
+            messageView.text="Hello! "+nameText.text
+        }
+        /*val messageView = findViewById<TextView>(R.id.greeting_text_view)
         val nameText = findViewById<EditText>(R.id.name_edit_text)
 
-        messageView.text = "Hello! "+ nameText.text
+        messageView.text = "Hello! "+ nameText.text*/
     }
 }
